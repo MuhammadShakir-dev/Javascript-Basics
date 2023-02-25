bf106bcdfb5256dcb49bee14d9d0259c8ae6c0ec
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-25 21:50:53 +0500

    code added

---
+++ js-commnets.js
@@ -0,0 +1,8 @@
+
+    <script>
+        document.write("Hello <br>") // this will print a hello on web browser.
+        document.write("This is Muhammad Shakir")
+         /* 
+                This will print This is Muhammad Shakir on the web browser.
+        */
+    </script>
