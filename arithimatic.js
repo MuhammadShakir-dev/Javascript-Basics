2e739bf08037f5175aa40514f2e6c5e81611e333
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-25 22:31:55 +0500

    code added

---
+++ arithimatic.js
@@ -0,0 +1,58 @@
+// addition
+let x = 12;
+let y = 24;
+let z = (x+y);
+
+document.write(z);
+
+// subtraction
+let x = 12;
+let y = 24;
+let z = (x-y);
+
+document.write(z);
+
+// multiplicaiton
+let x = 12;
+let y = 2;
+let z = (x*y);
+
+document.write(z);
+
+// division
+let x = 12;
+let y = 2;
+let z = (x/y);
+
+document.write(z);
+
+//exponentation
+let x = 12;
+let y = 2;
+let z = (x**y);
+
+document.write(z);
+
+// modulus (reminder)
+let x = 12;
+let y = 5;
+let z = (x%y);
+
+document.write(z);
+
+// incriment
+let x = 12;
+let y = 5;
+let z = (x+y);
+    z++
+
+document.write(z);
+
+// decriment
+let x = 12;
+let y = 5;
+let z = (x+y);
+    z--
+
+document.write(z);
+
