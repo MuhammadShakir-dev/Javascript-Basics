4b295472c1d216e51428216e16da9ee209c4f4e3
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-25 22:09:43 +0500

    code added

---
+++ let&const.js
@@ -0,0 +1,11 @@
+<script>
+     
+     let a = "Muhammad Shakir";
+         a = " Muhammad Shakir-dev ";
+         document.write(a);   // output is Muhammad Shakir-dev
+         
+      const b = "5th Feburary";
+            b = "6th";
+            document.write(b)    // error becuase const varibles are never decleared again
+ 
+</script>
