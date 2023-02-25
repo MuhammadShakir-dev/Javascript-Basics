8dc849d19608a71546fa350002bbd90f677f4ce3
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-25 22:24:36 +0500

    code added

---
+++ datatype.js
@@ -0,0 +1,11 @@
+let x = "Muhammad Shakir";
+    x = 32;
+    x = true;
+    x = false;
+    x = ["html" , "css" , "javascript"];
+    x = { first : "Muhammad" , last : "Shakir"};
+    x = undefined;
+    x ;
+ 
+    console.log(x);
+    console.log(typeof x);
