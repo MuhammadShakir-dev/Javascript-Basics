059b0ee7225c9d0fc922b761b8179b243898b975
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-25 22:40:52 +0500

    code added

--- assignment-operator.js
+++ assignment-operator.js
@@ -1,1 +1,43 @@
+// equal to operator.
+let x = 12;
+let y = 5;
+let z = (x+y);
+    z--
 
+document.write(z);
+
+// addition assignment operator.
+let x = 12;
+let y = 48;
+    x=x+y;
+console.log(x)
+
+// subtraction assignment operator.
+let x = 12;
+let y = 48;
+    x=x-y;
+console.log(x)
+
+// multiplication assignment operator.
+let x = 12;
+let y = 48;
+    x=x*y;
+console.log(x)
+
+//division assignment operator
+let x = 48;
+let y = 12;
+    x=x/y;
+console.log(x)
+
+// modulus assignment operator.
+let x = 48;
+let y = 12;
+    x=x%y;
+console.log(x)
+
+// exponential assignment operator.
+let x = 2;
+let y = 10;
+    x=x**y;
+console.log(x)
