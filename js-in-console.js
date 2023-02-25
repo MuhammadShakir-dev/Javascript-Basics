8fada2eee9490222f271387783be27ec82dbbc8f
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-25 22:54:10 +0500

    code added

---
+++ js-in-console.js
@@ -0,0 +1,13 @@
+     var x = 50; 
+        console.log(x);
+
+        console.log([1,2,3]);
+
+        console.table([1, 2, 3]);
+
+        console.error("Something went wrong."); 
+
+        console.time("Test");
+        console.warn("This is just warning");
+        console.timeEnd("Test");
+        //console.clear();
