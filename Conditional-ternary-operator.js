04bafd510c6267982b75168492f08b2db1897f94
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-27 00:40:42 +0500

    code added

---
+++ Conditional-ternary-operator.js
@@ -0,0 +1,7 @@
+// Work same as if else condition but a little bit different from that like in conditional ternary we cannot write more than one conditions and there
+// syntax are totally different.
+
+let today = "Monday";
+let className;
+className = "Yes it is" + " " + (today === "Monday" ? "True" : "False") + " today is game class";
+console.log(className);
