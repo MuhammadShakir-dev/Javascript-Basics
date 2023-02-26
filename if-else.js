7190da502aaba2442deaf6889c0f05f652808cb6
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-26 23:41:00 +0500

    code added

---
+++ if-else.js
@@ -0,0 +1,13 @@
+// if is a block of code that runs when a particular condition becomes trye.
+// else is also a block of code that runs when an if statment becomes false.
+
+let today = "monday";
+if(today === "monday"){
+    console.log("Game Development Class");
+}
+
+let x = 21;
+let y = 22;
+if(x < y){
+    console.log(typeof x);
+}
