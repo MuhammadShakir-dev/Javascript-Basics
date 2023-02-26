a21c46f6239998dc9471a4e02e587ec39bbab86b
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-26 23:37:08 +0500

    code added

--- if-statment.js
+++ if-statment.js
@@ -1,1 +1,14 @@
+// if is a block of code that runs when a particular condition becomes true.
+
+
+let today = "monday";
+if(today === "monday"){
+    console.log("Game Development Class");
+}
+
+let x = 21;
+let y = 22;
+if(x < y){
+    console.log(typeof x);
+}
 
