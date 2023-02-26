06f9ac2ed012c6d1f7fde614008a61f1db279ac2
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-27 01:20:49 +0500

    code added

---
+++ confirm-box.js
@@ -0,0 +1,10 @@
+///Confrim box are used for ask some type of questions fom users and on you have to answer it in yes or no.
+// on every yes or no you can procede any program further.
+
+let b = confirm("Do you know muhammad shakir-dev?");
+if(b){
+    alert("wohooo Thanks.")
+}
+else{
+    alert("Go and google it noww...!!!")
+}
