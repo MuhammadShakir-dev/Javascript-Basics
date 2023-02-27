9441590b8a19ef303f36555078e7326d99d713a1
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-27 22:57:32 +0500

    code added

--- JS-local-Global-var.js
+++ JS-local-Global-var.js
@@ -1,1 +1,24 @@
+// var a = 10; -- global varibles
+// function sum(num1,num2){
+//    var b = 25 --  local variables
+// }
+
+// Global variable :: You can acces a global variables inside and outside of the function.
+let a  = "Muhammad Shakir";
+function name(){
+console.log(a);
+}
+name();
+console.log(a);
+
+// local variable :  you cannot acces a local variale outside of the fuctioins.
+function community(){
+  let b = "Google Developer Stduent Club's";
+  console.log(b);
+}
+community();
+console.log(b);
+
+
+
 
