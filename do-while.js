f7b81ccb19b30641a5c6db6285ca885d5598c086
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-28 00:07:20 +0500

    code added

---
+++ do-while.js
@@ -0,0 +1,18 @@
+// do while loop works same as while loop but it  1 directly comes inside the loop and print the statment and from the second time it checkes the condition.
+
+// print talble of 2 using while loop.
+
+let start =  0;
+do{
+    console.log(start);
+    start = start + 2;
+}while(start <= 20);
+
+// print the reverse if table two
+
+let a = 20;
+do{
+    console.log(a);
+    a = a - 2;
+}while(a >= 1);
+
