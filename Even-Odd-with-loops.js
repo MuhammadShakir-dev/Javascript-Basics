b0958446d7254eb9fb32dde3a5ae3c85e16d654c
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-02-28 22:18:57 +0500

    Create Even-Odd-with-loops.js

---
+++ Even-Odd-with-loops.js
@@ -0,0 +1,17 @@
+// let's create a program that prints out even and odd number using loops;
+
+// create a program in js that prints all the even number betwwen 1 to 20 using loops.
+
+for(let a = 0; a<=20; a++){
+    if(a % 2 == 0){
+        console.log(a)
+    }
+}
+
+// write a program that print even number.
+
+for(let b = 0; b<=10; b++){
+    if(b % 2 != 0){
+        console.log(b)
+    }
+}
