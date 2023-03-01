cdc5c94734a62ab26bfc5501f8fa37fdf036c738
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-01 11:10:45 +0500

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,2 +1,17 @@
 // let's dirty our hands with Javascript arrays.
-// 
+
+// to initialize and print a simple array 
+var ary = [10,20,30,40,50];
+console.log(ary);
+
+// to print something specific from array.
+var ary = [10,20,30,40,50];
+console.log(ary[0],ary[3],ary[4]);
+
+// to print an array with a help of a loops .
+var ary = [10,20,30,40,50];
+var sum = 0;
+for(var a = 0; a <=4; a++){
+    sum = sum + ary[a];
+}
+    console.log(sum);
