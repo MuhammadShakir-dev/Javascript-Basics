aae6dbfab13a594f659b0c60774c780f79189d10
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-01 09:46:35 +0500

    Create nested-loop.js

---
+++ nested-loop.js
@@ -0,0 +1,11 @@
+// If you want to make a loop inside another loop than it's called nested loop.
+// wirte a program in javascript that prints counting from 1 to 100.
+ for( var a = 1; a <= 100; a = a+10){
+     for(var b = a; b < a+10; b++){
+         console.log(b + " ");
+     }
+     console.log("<br>");
+ }
+ 
+ // in this program i use nested loops for printing this program.
+ 
