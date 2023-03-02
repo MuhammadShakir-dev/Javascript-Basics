62e6dd31cc9592ee28ee227f87586493ee9e0b20
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-02 22:26:11 +0500

    Update concat-join.js

--- concat-join.js
+++ concat-join.js
@@ -1,1 +1,14 @@
+// concat means to add to different arrays.
+// in js you can concat arrays in different ways but i will tell you only how to concat arrays using just concat() function.
+// you can concat upto multiple arrays if you want.
 
+ let team1 = ["Shakir","Wajid","Haris"];
+ let team2 = ["Anum","Anas","Ammad"];
+ let team3 = ["jamal","hadi"];
+ 
+ console.log(team1);
+ console.log(team2);
+ 
+ let finalteam = team1.concat(team2,team3);
+ console.log(finalteam);
+ 
