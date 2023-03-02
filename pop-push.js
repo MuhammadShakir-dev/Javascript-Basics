c89efe27cc72d88de20f6618eea6067fba0a8d95
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-02 21:53:43 +0500

    Update pop-push.js

--- pop-push.js
+++ pop-push.js
@@ -1,1 +1,16 @@
+// if you want to delete last element of you array than you can use pop().
+// pop() only deletes the last element from you array.
+
+// if you want to add an element in your array at last then you can use push().
+// push can only add elements at last.
+
+
+let ary =  ["Javascript","Python","Ruby","Golang"];
+console.log(ary);
+
+ary.pop();
+console.log(ary);
+
+ary.push("Java");
+console.log(ary);
 
