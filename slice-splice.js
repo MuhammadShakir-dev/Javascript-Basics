a92c245580f548a54f3146b3f97f7ddb31a6d802
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-02 22:50:14 +0500

    Create slice-splice.js

---
+++ slice-splice.js
@@ -0,0 +1,10 @@
+// if you want to print out some specific values from array then use slice.
+//  slice has two parameters slice(start,end)
+// let's practice it. here i want to print out shakir, rehan, and saad so i give a starting point to it which is from index-1 and an ending point which
+// is index-4
+
+let ary = ["Hais","Shakir","Rehan","Saad","Wajid"];
+let f_ary = ary.slice(1,4);
+console.log(f_ary);
+
+//  if you don't want to add new values in the starting or at the ending of your array then use splice.
