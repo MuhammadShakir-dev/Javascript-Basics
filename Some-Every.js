60167e3e4b8f99885e2a5de5c99fc9beabdc5b75
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-04 14:21:58 +0500

    Update Some-Every.js

--- Some-Every.js
+++ Some-Every.js
@@ -1,1 +1,27 @@
+// check weather tha condition is ture or not.   
+// some return only ture and fasle boolean values.
+// some only work whne you call a fucntion with in that.
 
+let ages = [10, 12, 13, 17, 19];
+let adultAge = ages.some(checkAdult);
+console.log(adultAge);
+
+function checkAdult(age) {
+    return age >= 18 
+};
+
+// for fasle.
+// check weather tha condition is ture or not.   
+// some return only ture and fasle boolean values.
+// some only work whne you call a fucntion with in that.
+
+let ages = [10, 12, 13, 17, 11];
+let adultAge = ages.some(checkAdult);
+console.log(adultAge);
+
+function checkAdult(age) {
+    return age >= 18 
+};
+
+
+ 
