0fdfba4e68770c12effb3d365e143ea79757981e
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-04 15:56:38 +0500

    Create Array-Includes.js

---
+++ Array-Includes.js
@@ -0,0 +1,5 @@
+// Array includes check that the element you give it in the include function is present in the aray or not.
+
+var a = ["Sanjay", "Aman", "Rehman", "Rahul"];
+var b = a.includes("Aman");
+console.log(b)
