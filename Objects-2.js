bfad5ab289fda18588c71801d06c4fb2d145d585
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-07 11:29:25 +0500

    Update Objects-2.js

--- Objects-2.js
+++ Objects-2.js
@@ -1,1 +1,17 @@
+// A New way to create objects in javascript.
 
+// First initialize a variable and make that variable a new objects.
+let a  = new Object();
+
+// Now you can give variable a which is now an obejcts any properties or values you want at any time you want.
+
+a.firstName = "Muhammad",
+a.lastName  = "Shakir",
+a.skills = ["HTML","CSS","JavaScript","Tailwind CSS","Git","Ajax","WebAPI"],
+a.fullName = function(){
+    return this.firstName + " " + this.lastName;
+}
+
+// Now simply print the values.
+console.log(a.fullName());
+console.log(a.skills);
