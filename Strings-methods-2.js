8a0d1c63ae5335b2e86fa38c22950ff24aeb4e1e
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-09 13:09:21 +0500

    Update Strings-methods-2.js

--- Strings-methods-2.js
+++ Strings-methods-2.js
@@ -1,1 +1,15 @@
+// lets start with charAT.
+// in charAT you will define a position and at that position if any character is present then that character will be printed.
 
+       let name = "Muhammad Shakir";
+       let x = name.charAt(10);
+       console.log(x);
+
+// charCodeAt()
+        let userget = prompt("Enter an aplhabet or symbol : ");
+        let find = userget.charCodeAt();
+        console.log(find);
+
+// fromCharCode()
+        let find2 = String.fromCharCode(97);
+        console.log(find2);
