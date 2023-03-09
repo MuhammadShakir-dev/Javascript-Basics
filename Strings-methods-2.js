a0456ee71b260f3156dfdfc0c1af2c5a836c8369
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-09 13:32:17 +0500

    Update Strings-methods-2.js

--- Strings-methods-2.js
+++ Strings-methods-2.js
@@ -13,3 +13,17 @@
 // fromCharCode()
         let find2 = String.fromCharCode(97);
         console.log(find2);
+
+// repeat()
+// Repeat method or funciton are simply repeats or print your string mutiple time you to your given input.
+
+       let str = "hello world ";
+       let finalStr = str.repeat(10);
+       console.log(finalStr);
+
+// String split function or method split every word of a string and make a seperate array of that.
+
+       let str = "I'm a Front-end Engineer";  // string 
+       let finalStr = str.split(" ");  // split() function     
+       console.log(finalStr);  // Output
+       console.log(typeof(finalStr)); // Output "Object"
