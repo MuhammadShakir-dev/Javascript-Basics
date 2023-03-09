c504ac424f6a9da1148c7f112e9fd9969ff86993
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-09 22:49:41 +0500

    Create Number-Method.js

---
+++ Number-Method.js
@@ -0,0 +1,6 @@
+// there are seven methods that are used in number methods.
+// number methods in js
+
+  let a = prompt("Enter a number i will add +10 in that number : ");
+  let num = Number(a);
+  console.log( num + 10);
