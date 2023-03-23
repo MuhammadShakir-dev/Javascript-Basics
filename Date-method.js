e6dcafcf92f0e9492b4287e7f647ee3b123c3cab
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-23 11:33:29 +0500

    Update Date-method.js

--- Date-method.js
+++ Date-method.js
@@ -9,3 +9,28 @@
 
   // toDateString for converting date into readable format.
   console.log(demoDate.toDateString());
+
+  // date methods in javascript
+
+  let dateDemo = new Date();
+
+  // to see the date in proper order
+  console.log(dateDemo.toDateString());
+
+  // to just print the date
+  console.log(dateDemo.getDate());
+
+  // to get full year
+  console.log(dateDemo.getFullYear());
+
+  // to get month
+  // for january its 0 and for december its 11
+  console.log(dateDemo.getMonth());
+
+  // to get day
+  // for monday it's 1 and for sunday it's 1
+  // monday 1
+  // tues 2
+  // wed 3
+  // thursday 4
+  console.log(dateDemo.getDay());
